b1dc9ffa73525f244ecff0003f4fa3664666b7fa
PRK7 <42325085+PRK7@users.noreply.github.com>
2023-01-27 07:36:47 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -10,10 +10,10 @@ print(x)
 print('class: 0-Iris-Setosa, 1- Iris-Versicolour, 2- Iris-Virginica')
 print(y)
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3)
-#To Training the model and Nearest nighbors K=5
+
 classifier = KNeighborsClassifier(n_neighbors=5)
 classifier.fit(x_train, y_train)
-#To make predictions on our test data
+
 y_pred=classifier.predict(x_test)
 print('Confusion Matrix')
 print(confusion_matrix(y_test,y_pred))
