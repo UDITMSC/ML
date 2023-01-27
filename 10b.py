01c90fe57e9a9d57d5109f63ee9b7befebf31aaa
PRK7 <42325085+PRK7@users.noreply.github.com>
2023-01-27 07:38:32 +0530

    Update 10b.py

--- 10b.py
+++ 10b.py
@@ -4,27 +4,26 @@ from bokeh.layouts import gridplot
 from bokeh.io import push_notebook
 
 
-def local_regression(x0, X, Y, tau):  # add bias term
-    x0 = np.r_[1, x0]  # Add one to avoid the loss in information
+def local_regression(x0, X, Y, tau):  
+    x0 = np.r_[1, x0]  
     X = np.c_[np.ones(len(X)), X]
 
-    # fit model: normal equations with kernel
-    xw = X.T * radial_kernel(x0, X, tau)  # XTranspose * W
 
-    beta = np.linalg.pinv(xw @ X) @ xw @ Y  # @ Matrix Multiplication or Dot Product
+    xw = X.T * radial_kernel(x0, X, tau)  
 
-    # predict value
-    return x0 @ beta  # @ Matrix Multiplication or Dot Product for prediction
+    beta = np.linalg.pinv(xw @ X) @ xw @ Y  
+
+
+    return x0 @ beta  
 
 
 def radial_kernel(x0, X, tau):
     return np.exp(np.sum((X - x0) ** 2, axis=1) / (-2 * tau * tau))
 
 
-# Weight or Radial Kernal Bias Function
 
 n = 1000
-# generate dataset
+
 X = np.linspace(-3, 3, num=n)
 print("The Data Set ( 10 Samples) X :\n", X[1:10])
 Y = np.log(np.abs(X ** 2 - 1) + .5)
@@ -38,7 +37,6 @@ print(" Xo Domain Space(10 Samples) :\n", domain[1:10])
 
 
 def plot_lwr(tau):
-    # prediction through regression
     prediction = [local_regression(x0, X, Y, tau) for x0 in domain]
     plot = figure(width=400, height=400)
     plot.title.text = 'tau=%g' % tau
